9d418e1d930266c7c512355436519c694bf5e451
girordo <tsgiroldo@gmail.com>
2023-01-09 23:34:37 -0300

    :robot: | adding some tests

--- lib/password_generator.ex
+++ lib/password_generator.ex
@@ -1,17 +1,4 @@
 defmodule PasswordGenerator do
-  @moduledoc """
-  Documentation for `PasswordGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PasswordGenerator.hello()
-      :world
-
-  """
   def hello do
     :world
   end
